1fb1410e2b3f01c3d446603a5fa81b9079a256d6
Emmalyn Campau <ecampau@umich.edu>
2025-02-23 19:29:00 -0500

    Add comments

--- VirusHostNetworkAnalysis/null_model.py
+++ VirusHostNetworkAnalysis/null_model.py
@@ -1,33 +1,42 @@
 import numpy as np
 import networkx as nx
 
-## ER Class
 class ER:
+    """ Class to create a random graph using the Erdős-Rényi model. 
+    The graph is initialized with a given number of rows and columns, and a probability p for edge creation.
+    
+    Args:
+    rows (int): Number of rows in the graph.
+    columns (int): Number of columns in the graph.
+    p (float): Probability of edge creation between nodes.
+
+    """
     def __init__(self, rows:int, columns:int, p:float):
         self.rows = list(range(1, rows))
         self.columns = list(range(rows, rows + columns))
         self.p = p
 
     def initialize_graph(self):
+        """ Initialize the graph with nodes and edges. """
         self.G = nx.Graph()
         self.G.add_nodes_from(self.rows)
         self.G.add_nodes_from(self.columns)
-        print(self.rows)
-        print(self.columns)
 
-    # Create a graph with n nodes and random edges between them
     def fill_ER_graph(self):
-        # loop through the nodes, rows then columns
+        """ Create a graph with n nodes and random edges between them."""
+
+        # Iterate through all pairs of nodes in the graph
         for row in self.rows:
             for col in self.columns:
                 if row != col:
                     # set p to a random value between 0 and 1
                     p_rand = np.random.rand()
-                    # if p is less than 0.1, add an edge between the nodes
+                    # if the random probability is less than p, add an edge between the nodes
                     if p_rand < self.p:
                         self.G.add_edge(row, col)
 
     def draw_graph(self):
+        """ Draw the graph using NetworkX. """
         self.initialize_graph()
         self.fill_ER_graph()
         # Draw the graph
@@ -43,3 +52,6 @@ class ER:
         self.eigenvector = nx.eigenvector_centrality(self.G)
         self.pagerank = nx.pagerank(self.G)
         print(self.degree)
+
+
+# Configuration Model 
--- VirusHostNetworkAnalysis/prediction_matrix.py
+++ VirusHostNetworkAnalysis/prediction_matrix.py
@@ -1,38 +1,49 @@
+from os import replace
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-## Square Matrix Class
+
 class PredictionMatrix:
+    """ Class to make a matrix from the input file. Can make square or rectangle matrices. Can also be used to make probabilty matrix.
+    
+    Args:
+    vhip_predictions_file (str): Path to the input file containing virus-host interaction predictions.
+    
+    """
+
     def __init__(self, vhip_predictions_file:str):
         self.file = vhip_predictions_file
         self.virus_host = pd.read_csv(self.file, sep='\t')
         self.predictions = self.virus_host[self.virus_host['Predictions'] == 1]
 
-    # Find the unique virus names and host names in the dataset. The first column contains a virus name and host name, which are separated by a colon.
+
     def get_unique_virus_host(self):
+        """Find the unique virus names and host names in the dataset. The first column contains a virus name and host name, which are separated by a colon."""
         self.unique_viruses = self.virus_host['pairs'].str.split(':').str[0].unique()
         self.unique_hosts = self.virus_host['pairs'].str.split(':').str[1].unique()
-        print('Unique viruses and hosts found')
 
-    # Save the matrix to a csv file
+
     def save_matrix(self):
+        """ Save the matrix to a csv file. """
         np.savetxt('virus_host.csv', self.virus_host_array, delimiter=',')
         print('virus_host.csv saved')
 
     # Create a matrix full of zeros and make a list of rows and column labels
     def initialize_matrix(self, matrix_type:str):
+        """"Create a matrix full of zeros and make a list of rows and column labels.
+        
+        Args:
+        matrix_type (str): Type of matrix to create, which determines if the values are boolean or float.
+        
+        """""
         self.rows = self.unique_viruses
         self.columns = self.unique_hosts
-        if (matrix_type == "prediction"):
-            self.virus_host_array = np.zeros((len(self.rows), len(self.columns)), dtype=bool)
-        else:
-            self.virus_host_array = np.zeros((len(self.rows), len(self.columns)), dtype=float)
-        print('Matrix initialized') 
+        self.virus_host_array = np.zeros((len(self.rows), len(self.columns)), dtype= bool if matrix_type == "prediction" else float)
     
     def fill_matrix(self, matrix_type:str):
-        # Loop through the rows in subset of data where predictions is 1
+        """ Fill the prediction matrix with 1s and 0s or fill the probability matrix with the InfProbabilities from the dataset."""
         if matrix_type == "prediction":
             for _, row in self.predictions.iterrows():
                 virus, host = row['pairs'].split(':')
@@ -51,57 +62,62 @@ class PredictionMatrix:
                 self.virus_host_array[virus_index][host_index] = row['InfProbabilities']
                 #print(row['InfProbabilities'])
 
-        print('Matrix filled')
 
     def sort_rows(self):
-        # Count the 1s in each row
+        """ Find the sum of each row and move rows with the highest sum to the top of the matrix. """
         row_counts = np.sum(self.virus_host_array, axis=1)
         # Sort inidices based on the counts
         sorted_indices = np.argsort(row_counts)[::-1]
         # Sort the matrix and row names based on the sorted indices
         self.virus_host_array = self.virus_host_array[sorted_indices]
         self.rows = self.rows[sorted_indices]
-        print('rows sorted')
 
     def sort_columns(self):
-        # Count the 1s in each column
+        """ Find the sum of each column and move columns with the highest sum to the left of the matrix. """
         col_counts = np.sum(self.virus_host_array, axis=0)
         # Sort inidices based on the counts
         sorted_indices = np.argsort(col_counts)[::-1]
         # Sort the matrix and column names based on the sorted indices
         self.virus_host_array = self.virus_host_array[:, sorted_indices]
         self.columns = self.columns[sorted_indices]
-        print('columns sorted')
 
     def sort_matrix(self):
         self.sort_rows()
         self.sort_columns()
 
-    # Make the matrix a square by adding rows for the hosts and columns for the viruses
     def expand_matrix(self, matrix_type:str):
+        """ Expand the matrix to make it square by adding rows for the hosts and columns for the viruses.
+        
+        Args:
+        matrix_type (str): Type of matrix to create, which determines if the values are boolean or float.
+        
+        """
         # add number of hosts to the rows
-        self.virus_host_array_square = np.concatenate((self.virus_host_array, np.zeros((len(self.unique_hosts), len(self.columns)), dtype=bool)), axis=0)
+        self.virus_host_array_square = np.concatenate((self.virus_host_array, np.zeros((len(self.unique_hosts), len(self.columns)), dtype=bool if matrix_type == "prediction" else float)), axis=0)
         self.rows_square = np.concatenate((self.rows, self.unique_hosts))
         # add number of viruses to the columns
-        self.virus_host_array_square = np.concatenate((self.virus_host_array_square, np.zeros((len(self.rows_square), len(self.unique_viruses)), dtype=bool)), axis=1)
+        self.virus_host_array_square = np.concatenate((self.virus_host_array_square, np.zeros((len(self.rows_square), len(self.unique_viruses)), bool if matrix_type == "prediction" else float)), axis=1)
         self.columns_square = np.concatenate((self.columns, self.unique_viruses))
         # Fill with 1s
         self.fill_bottom_right(matrix_type)
 
     def fill_bottom_right(self, matrix_type:str):
-        # fill the virus-host pairs with same values as host-virus pairs
-        for index, row in self.predictions.iterrows():
+        """ Fill the virus-host pairs with same values as host-virus pairs. Use the predictions or the InfProbabilities from the dataset."""
+
+        if matrix_type == "prediction":
+            arrayname = self.predictions
+        else:
+            arrayname = self.virus_host
+
+        for _, row in arrayname.iterrows():
             virus, host = row['pairs'].split(':')
             virus_index = np.where(self.columns_square == virus)[0][0]
             host_index = np.where(self.rows_square == host)[0][0]
-            if matrix_type == "prediction":
-                self.virus_host_array_square[host_index][virus_index] = 1
-            else:
-                self.virus_host_array_square[host_index][virus_index] = row['InfProbabilities']
+            self.virus_host_array_square[host_index][virus_index] = 1 if matrix_type == "prediction" else row['InfProbabilities']
         return self.virus_host_array_square
     
-    # Make rectangular matrix
     def make_rectangular_matrix(self, matrix_type:str):
+        """ Call all functions to make a rectangular matrix"""
         matrix_type1 = matrix_type.lower()
         self.get_unique_virus_host()
         self.initialize_matrix(matrix_type1)
@@ -109,19 +125,21 @@ class PredictionMatrix:
         self.sort_matrix()
         return self.virus_host_array
 
-    # Make square matrix
     def make_square_matrix(self, matrix_type:str):
+        """ Call all functions to make a square matrix"""
         self.make_rectangular_matrix(matrix_type.lower())
         self.expand_matrix(matrix_type.lower())
         return self.virus_host_array_square
 
-    # Plot the heatmap for the rectangular matrix
     def plot_heatmap(self, matrix_type:str):
         # if matrix type prediction, use purples, otherwise use warm colors
         sns.heatmap(self.virus_host_array, cmap='Purples' if matrix_type == 'prediction' else 'YlOrRd')
         plt.gcf().set_size_inches(7, 14)
         plt.xlabel("Hosts")
         plt.ylabel("Viruses")
-        plt.savefig("heatmap.png")
+        matrix_title = self.file.replace('Sample_Input/', '')
+        plt.title(matrix_title)
+        # save the figure in the heatmaps folder
+        plt.savefig('Heatmap_' + matrix_type + '_' + matrix_title + '.png')
         plt.show()
 
